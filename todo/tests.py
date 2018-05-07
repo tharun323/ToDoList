3a2ae22f5220c528cba79bd55e65322870adf75e
tharun323 <atkr323@gmail.com>
2018-05-07 10:45:02 +0530

    Tests are added

--- todo/tests.py
+++ todo/tests.py
@@ -1,5 +1,61 @@
 from django.test import TestCase
-import datetime
+from todo.models import Task
 from django.utils import timezone
-from django.test import TestCase
+from django.urls import reverse
+from todo.forms import TodoForm
+from todo.views import Home
+
+#tesing djnago models
+class ToDoModelTest(TestCase):
+
+    def CreateTask(self,title="watch today",due_date="2018-02-02"):
+        return Task.objects.create(title=title, due_date=due_date)
+
+    def TestCreateTask(self):
+        w=self.CreateTask()
+        self.assertTrue(isinstance(w,Task))
+        self.assertEqual(w.__unicode__(),w.title)
+#End of django models Testing.................................
+
+#testing views .................................................
+    def TestHomeView(self):
+        w=self.CreateTask()
+        url=reverse('Home')
+        resp=self.client.get(url)
+
+        self.assertEqual(resp.status_code,200)
+        self.assertIn(w.title,resp.content)
+
+    def TestdeleteView(self):
+        w = self.CreateTask()
+        url = reverse('delete')
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(w.title, resp.content)
+
+   def TestmonthView(self):
+        w=self.CreateTask()
+        url=reverse('nweek')
+        resp=self.client.get(url)
+
+        self.assertEqual(resp.status_code,200)
+        self.assertIn(w.title,resp.content)
+
+   def TesttodayView(self):
+        w=self.CreateTask()
+        url=reverse('today')
+        resp=self.client.get(url)
+
+        self.assertEqual(resp.status_code,200)
+        self.assertIn(w.title,resp.content)
+
+   def TestweekView(self):
+        w=self.CreateTask()
+        url=reverse('week')
+        resp=self.client.get(url)
+
+        self.assertEqual(resp.status_code,200)
+        self.assertIn(w.title,resp.content)
+#testing views completed.......................................
 
